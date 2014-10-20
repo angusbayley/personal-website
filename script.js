4ba294b3dfde351b3db15eb809d91f9e07611505
angusbayley <angusbayley@gmail.com>
2014-10-20 21:17:19 +0100

    fixing accordion bug and smoothing out animation

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
 $(document).ready(function(){
+
+	//MOUSEOVER EFFECTS
 	$('.header').mouseenter(function(){
 		$(this).fadeTo(150,1);
 	});
 	$('.header').mouseleave(function(){
 		$(this).fadeTo(150,0.5);
 	});
+
+	//HOMEMADE ACCORDION
+	var nDropdowns = $('.dropdown').length;
+	var ddOpen = [];
+	for (n=0; n<nDropdowns; n++) {
+		ddOpen.push($('.dropdown').eq(n).is(':visible'));
+	}
+
 	$('.header').click(function(){
 		$('.dropdown').slideUp('fast');
 		var index = $('.header').index(this);
-		if (!ddClicked()) {
+		if (!ddClicked(ddOpen)) {
 			$('.dropdown').eq(index).slideToggle('fast');
+			reset(ddOpen, nDropdowns);
+			ddOpen[index] = true;
+		}
+		else if (ddOpen[index]) {
+			ddOpen[index] = false;
+			return
 		}
 		else {
+			reset(ddOpen, nDropdowns);
+			ddOpen[index] = true;
 			$('.dropdown').eq(index).delay('200').slideToggle("fast");
 		}
 	});
-	$('#gizmos span').click(function(){
-		//alert('clicked the span');
-		$('#gizmosDropdown').slideToggle("fast");
-	});
 });
 
-ddClicked = function() {
-	var nDropdowns = $('.dropdown').length;
-	var checkArray = [];
-	for (n=0; n<nDropdowns; n++) {
-		checkArray.push($('.dropdown').eq(n).is(':visible'));
-	}
-	var allTrue = checkArray.some(function(item, index, array) {return item});
+reset = function(dd, nD) {
+	for (i=0; i<nD; i++) {
+		dd[i] = false;
+	} 
+}
+
+ddClicked = function(dd) {
+	var allTrue = dd.some(function(item, index, array) {return item});
 	return allTrue
 }
